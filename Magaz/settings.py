33ea95ed0410ebfb8ab83f8393b7a1f493d6c236
Amter <dooufupv@gmail.com>
2024-05-15 09:07:17 +0300

    1.1

--- Magaz/settings.py
+++ Magaz/settings.py
@@ -84,10 +84,15 @@ WSGI_APPLICATION = 'Magaz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Amter',
+        'USER': 'Amter',
+        'PASSWORD': '1982',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
